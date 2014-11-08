16bea01e3f9b0e0ca8fe2978e70e8a1728041c25
Ivan Gonzalez <iglpdc@gmail.com>
2014-11-08 15:46:20 -0500

    Finish script for adding data files.

--- src/add_data.sh
+++ src/add_data.sh
@@ -1,3 +1,7 @@
 DRUG=$(python assign_drug.py $2)
 DEST=data/$1/$1-$DRUG.dat
-echo cp $2 $DEST
+cp $2 $DEST
+git add $DEST
+MSG="Add file $DEST"
+git commit -m "'$MSG'"
+echo "New file added to the repo: $DEST"
