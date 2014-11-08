5e5f0c052d0f6853c83ddd09a0c922d0b7bd32bb
Ivan Gonzalez <iglpdc@gmail.com>
2014-11-08 15:01:31 -0500

    Add script to assing drug

--- src/assign_drug.py
+++ src/assign_drug.py
@@ -7,7 +7,12 @@ def assign_drug(filename):
         result = 'placebo'
     return result
 
+import sys
+
+filename = sys.argv[1] 
+print assign_drug(filename)
     
-assert assign_drug("inflammation_1.dat") == "tylenol"
-assert assign_drug("inflammation_4.dat") == "placebo"
+assert assign_drug("inflammation_1.dat") == "tylenol" 
+assert assign_drug("inflammation_4.dat") == "placebo" 
 assert assign_drug("inflammation_3.dat") == "tylenol"
+
